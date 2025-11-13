70788802b49b9370a89d22211b8497226ab7f8d6
Mati9284 <matiaseest5@gmail.com>
2025-11-13 09:46:31 -0300

    Add files via upload

---
+++ L.P MAIN TP 8 2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <ctype.h>
+using namespace std;
+int main ()
+{
+	setlocale(LC_ALL, "spanish");
+	int v[4];
+	int valor=0, total=0,prom=0,retornar=0;
+	cout << "Este programa indica la suma de todos los elementos de un vector de números enteros" << endl;
+	cout << "Ingrese cuántos valores de vectores quiera ingresar" << endl;
+	cin >> valor;
+	for (int i=0;i<valor;i++)
+	{
+		cout << "Ingrese primer valor" << endl;
+		cin >> v [valor];
+		total = total + v[valor];
+	}
+	prom = total/valor;
+	cout << "El resultado de la suma es: " << total << endl;
+	cout << "El resultado del promedio es: " << prom << endl;
+	cout << "¿Quieres volver a iniciar el programa? (si 1 no 2)" << endl;
+	cin >> retornar;
+	if (retornar==1)
+	{
+		return main ();
+	}
+}
